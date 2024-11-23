84fefa38240d7191668a236e1840dc5f8182c05b
Guillermo <fuentesbuenosvinosguillermo@gmail.com>
2024-11-23 10:30:55 +0100

    actualizacion

--- app/src/main/java/com/fuentesbuenosvinosguillermo/pokedex/Fragments/pokemonCapturados.java
+++ app/src/main/java/com/fuentesbuenosvinosguillermo/pokedex/Fragments/pokemonCapturados.java
@@ -1,14 +1,14 @@
 package com.fuentesbuenosvinosguillermo.pokedex.Fragments;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.fuentesbuenosvinosguillermo.pokedex.ConfiguracionRetrofit.Pokemon;
 import com.fuentesbuenosvinosguillermo.pokedex.RecyclerViewCapturados.AdapterCapturados;
@@ -16,18 +16,11 @@ import com.fuentesbuenosvinosguillermo.pokedex.LogicaCapturaCompartida.SharedVie
 import com.fuentesbuenosvinosguillermo.pokedex.databinding.FragmentPokemonCapturadosBinding;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class pokemonCapturados extends Fragment {
     private FragmentPokemonCapturadosBinding binding;
     private AdapterCapturados adapterCapturados;
-    private final List<Pokemon> pokemonCapturadosList = new ArrayList<>(); // Lista de capturados
 
-
-
-
-
-    // Asegúrate de cargar la lista de Pokémon capturados
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentPokemonCapturadosBinding.inflate(inflater, container, false);
@@ -36,38 +29,20 @@ public class pokemonCapturados extends Fragment {
         setupRecyclerView();
 
         // Observar cambios en los Pokémon capturados usando el SharedViewModel
-        //Inicializa SharedViewModel
         SharedViewModel sharedViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
-        //llama a un metodo que hemos creado en sharedViewModel el cual es una lista dinamica y a esta lista
-        //le pasamos un observador 'observe' que detectara los cambios en dicha lista ha este obsevador le pasamos
-        //el ciclo de vida de la vista con getViewLifecycleOwner() que puede tener estos ciclos -->
-        /*
-        * Namely, the lifecycle of the Fragment's View is:
-            created after onViewStateRestored(Bundle) --> creado
-            started after onStart()--> empezado
-            resumed after onResume()-->mostrado
-            paused before onPause()-->pausado
-            stopped before onStop()-->parado
-            destroyed before onDestroyView()-->destruido
-        *
-        * */
-        //luego le pasamos el nombre de la variable que retorna getCapturedPokemons() que es capturedPokemons y finalmente
-        //esto va al adaptador y actualiza los datos con los nuevos pokemons
         sharedViewModel.getCapturedPokemons().observe(getViewLifecycleOwner(), capturedPokemons -> {
             adapterCapturados.updateData(capturedPokemons);
         });
-
         return binding.getRoot();
     }
 
-
-
     private void setupRecyclerView() {
         binding.pokemonsCapturadosRecyclerview.setLayoutManager(new LinearLayoutManager(getContext()));
-        adapterCapturados = new AdapterCapturados(new ArrayList<>()); // Inicializa con una lista vacía
+
+        // Configurar el adaptador sin listener
+        adapterCapturados = new AdapterCapturados(new ArrayList<>());
+
         binding.pokemonsCapturadosRecyclerview.setAdapter(adapterCapturados);
     }
-
-
 
 }
--- app/src/main/java/com/fuentesbuenosvinosguillermo/pokedex/RecyclerViewCapturados/AdapterCapturados.java
+++ app/src/main/java/com/fuentesbuenosvinosguillermo/pokedex/RecyclerViewCapturados/AdapterCapturados.java
@@ -2,10 +2,10 @@ package com.fuentesbuenosvinosguillermo.pokedex.RecyclerViewCapturados;
 
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 
 import com.fuentesbuenosvinosguillermo.pokedex.ConfiguracionRetrofit.Pokemon;
 import com.fuentesbuenosvinosguillermo.pokedex.databinding.PokemonCapturadosCardviewBinding;
@@ -15,10 +15,13 @@ import java.util.List;
 public class AdapterCapturados extends RecyclerView.Adapter<ViewHolderCapturados> {
     private final List<Pokemon> capturadosList;
 
-    // Constructor para recibir la lista de Pokémon capturados
+
+    // Constructor para recibir la lista de Pokémon capturados y el listener
     public AdapterCapturados(List<Pokemon> capturadosList) {
         this.capturadosList = capturadosList;
+
     }
+
     public void updateData(List<Pokemon> newPokemonList) {
         capturadosList.clear(); // Limpia la lista actual
         capturadosList.addAll(newPokemonList); // Agrega los nuevos datos
@@ -44,10 +47,17 @@ public class AdapterCapturados extends RecyclerView.Adapter<ViewHolderCapturados
 
         // Vincular los datos del Pokémon al ViewHolder
         holder.bind(pokemon);
+        holder.itemView.setOnClickListener(v->{
+            Toast.makeText(holder.itemView.getContext(), "Clic en " + pokemon.getName(), Toast.LENGTH_SHORT).show();
+        });
+
     }
+
 
     @Override
     public int getItemCount() {
         return capturadosList.size();
     }
+
+
 }
